2b9b4c28d5d5a159ec548f879a57f3fab6f496f4
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2025-05-02 00:03:16 +0600

    Change Project Structure

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const { dbConnect } = require("./config/dbConnect");
+const { dbConnect } = require("./src/config/db.connect");
 const cors = require("cors");
 dotenv.config();
-const app = require("./app");
+const app = require("./src/app");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,8 +14,8 @@ app.use(express.json());
 dbConnect();
 
 // server
-const port = process.env.PORT || 5100;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Chat Server is running on port ${port}`);
+  console.log(`Task Management Server is running on port ${port}`);
 });
